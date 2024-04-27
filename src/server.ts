25fc26e92494f5b4ab2c02a7054b54f68a44de5a
Lucas Araujo <lucasaraujo4188@gmail.com>
2024-04-26 22:42:19 -0300

    error handling

--- src/server.ts
+++ src/server.ts
@@ -14,49 +14,71 @@ const PORT = 3333
 let tasks: Task[] = []
 
 app.post('/tasks', (request: Request, response: Response) => {
-    const { name } = request.body as RequestBody
-    const task = new Task(name)
-    tasks.push(task)
-    return response.status(201).json({ message: "Tarefa criada com sucesso" })
+    try {
+        const { name } = request.body as RequestBody
+        const task = new Task(name)
+        tasks.push(task)
+        return response.status(201).json({ message: "Tarefa criada com sucesso" })
+    } catch (error) {
+        return response.status(500).json({ message: "Houve um erro inesperado" })
+    }
+
 });
 
 app.get('/tasks/:id', (request: Request, response: Response) => {
-    const { id } = request.params
-    const task = tasks.find((item) => item.id === id)
+    try {
+        const { id } = request.params
+        const task = tasks.find((item) => item.id === id)
 
-    if (typeof task === 'undefined') {
-        return response.status(404).json({ message: "Tarefa não encontrada" })
+        if (typeof task === 'undefined') {
+            return response.status(404).json({ message: "Tarefa não encontrada" })
+        }
+
+        return response.status(200).json({ task })
+    } catch (error) {
+        return response.status(500).json({ message: "Houve um erro inesperado" })
     }
 
-    return response.status(200).json({ task })
 });
 
 
 app.get('/tasks', (request: Request, response: Response) => {
-    return response.status(200).json({ tasks })
+    try {
+        return response.status(200).json({ tasks })
+    } catch (error) {
+        return response.status(500).json({ message: "Houve um erro inesperado" })
+    }
 });
 
 
 app.put('/tasks/:id', (request: Request, response: Response) => {
-    const { id } = request.params
-    const { name } = request.body
+    try {
+        const { id } = request.params
+        const { name } = request.body
 
-    const task = tasks.find((item) => item.id === id)
+        const task = tasks.find((item) => item.id === id)
 
-    if (typeof task === 'undefined') {
-        return response.status(404).json({ message: "Tarefa não encontrada" })
+        if (typeof task === 'undefined') {
+            return response.status(404).json({ message: "Tarefa não encontrada" })
+        }
+
+        task.name = name
+
+        return response.status(200).json({ message: "Tarefa editada com sucesso" })
+    } catch (error) {
+        return response.status(500).json({ message: "Houve um erro inesperado" })
     }
-
-    task.name = name
-
-    return response.status(200).json({ message: "Tarefa editada com sucesso" })
 });
 
 
 app.delete('/tasks/:id', (request: Request, response: Response) => {
-    const { id } = request.params
-    tasks = tasks.filter((item) => item.id !== id)
-    return response.status(200).json({ message: "Tarefa excluída com sucesso!" })
+    try {
+        const { id } = request.params
+        tasks = tasks.filter((item) => item.id !== id)
+        return response.status(200).json({ message: "Tarefa excluída com sucesso!" })
+    } catch (error) {
+        return response.status(500).json({ message: "Houve um erro inesperado" })
+    }
 });
 
 app.listen(PORT, () => {
